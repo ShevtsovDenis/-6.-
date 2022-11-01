878fd5b9a4554ed28719bef01ced36150a9cb284
ShevtsovDenis <shevcov.organika@yandex.ru>
2022-11-02 00:33:07 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение из слов, разделенных пробелами и без знаков припенания.");
+            string start_string = Console.ReadLine();
+            //преобразуем строку в массив//
+            string[] string_array = start_string.Split();
+            //присваиваем переменной значение первого слова в массиве
+            string max = string_array[0];
+            //находим самое длинное слово//
+            foreach (string s in string_array)
+            {
+                if (s.Length > max.Length)
+                {
+                    max = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в предложении - {0}",max);
+            Console.WriteLine("Для завершения нажмите любую клавишу на клавиатуре.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение из слов, разделенных пробелами и без знаков припенания.");
+            string start_string = Console.ReadLine();
+            string[] string_array = start_string.Split();
+            string result_string = "";
+            //пишем строку в обратном порядке//
+            foreach (char c in start_string)
+            {
+                result_string = c + result_string;
+            }
+            //убираем влияние пробелов и регистра в сторках//
+            result_string = result_string.ToLower().Replace(" ", "");
+            start_string = start_string.ToLower().Replace(" ", "");
+            //для визуального контроля//
+            Console.WriteLine(result_string);
+            Console.WriteLine(start_string);
+            //сравниваем получившиеся строки//
+            if (start_string == result_string)
+            {
+                Console.WriteLine("Выражение является полиндромом.");
+            }
+            else
+            {
+                Console.WriteLine("Выражение не является полиндромом.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
